9ece830f3057b3b15c4c07a67bd0bf8e4f6b3b03
Gabriele <gabriele.miotti2@gmail.com>
2021-12-23 22:23:26 +0100

    merge to one file

--- src/Solver.java
+++ src/Solver.java
@@ -7,8 +7,6 @@ import java.util.PriorityQueue;
 
 public class Solver {
 
-    public static int boardSize;
-
     public static void main(String[] args) {
 
         String fileName = null;
@@ -17,6 +15,7 @@ public class Solver {
             fileName = args[0];
         } catch (Exception e) {
             System.out.println("Missin file name!!");
+            return;
         }
 
         String boardInputString = null;
@@ -30,14 +29,28 @@ public class Solver {
             reader.close();
         } catch (FileNotFoundException e) {
             System.out.println("File Not Found!");
+            return;
         }
+
+        Solver solve = new Solver();
+
+        solve.solveBoard(boardInputString);
+
+    }
+
+    public static int boardSize;
+
+    Solver() {}
+
+    void solveBoard(String strBoard) {
 
         boolean endFound = false;
 
         HashMap<Board, Node> nodeMap = new HashMap<>();
+        //HashMap<long[], Board> nodeMap = new HashMap<>();
 
         //Create the first node with zero moves
-        Node node = new Node(boardInputString, null, 0); 
+        Node node = new Node(strBoard, null, 0); 
         
         endFound = node.checkEnd();;
 
@@ -93,6 +106,94 @@ public class Solver {
 
         while(!strBoards.isEmpty()) {
             System.out.println(strBoards.removeLast());
+        }
+    }
+
+    private static int[][] getTiles(String board) {
+        String[] chars = board.split(" ");
+        int[][] tiles = new int[Solver.boardSize][Solver.boardSize];
+        int i = 0;
+        int j = 0;
+        for(String c : chars) {
+            tiles[i][j++] = Short.parseShort(c);
+
+            if(j >= Solver.boardSize) {
+                i++;
+                j = 0;
+            }
+        }
+        return tiles;
+    }
+
+    class Node implements Comparable<Node>{
+    
+        private Node previous;
+        private Board board;
+        private int moves;
+        private int score;
+    
+        public Node(String strBoard, Node previous, int moves) {
+            this.board = new Board(getTiles(strBoard));
+            this.previous = previous;
+            this.moves = moves;
+            this.score = this.board.getManDist() + this.board.getLinConflit() + this.moves;
+        }
+        
+        //this constructuor is only called from inside so it could be private
+        private Node(Board board, Node previous, int moves) {
+            this.board = board;
+            this.previous = previous;
+            this.moves = moves;
+            this.score = this.board.getManDist() + this.board.getLinConflit() + this.moves;
+        }
+    
+        public Node getPrevious() {
+           return previous; 
+        }
+    
+        public int getMoves() {
+            return moves;
+        }
+    
+        public void setMoves(int m) {
+            this.moves = m;
+        }
+    
+        public Board getBoard()  {
+            return board;
+        }
+    
+        public boolean checkEnd() {
+            return this.board.getManDist() == 0;
+            //return Arrays.equals(Board.endBoard, this.board.getCTiles());
+        }
+    
+        @Override
+        public int compareTo(Node o) {
+            return this.score - o.score;
+        }
+    
+        @Override
+        public boolean equals(Object o) { 
+            return this.score == ((Node)o).score;
+        }
+    
+        public LinkedList<Node> getSons() {
+            LinkedList<Node> sons = new LinkedList<>();
+            //get a list with all the son of the current board
+            LinkedList<Board> moves = board.getMoves();
+    
+            //adding one move for the all the son
+            int sonsMoves = this.moves + 1;
+    
+            //System.out.println("Dead: " + board + " manhattan: " + board.getManDist());
+    
+            for(int i = 0; i < moves.size(); i++) {
+                sons.add(new Node(moves.get(i), this, sonsMoves));
+                //System.out.println("Son: " + moves.get(i) + " manhattan: " + (moves.get(i).getManDist() + moves.get(i).getLinConflit() + sonsMoves));
+            }
+    
+            return sons;
         }
     }
 
